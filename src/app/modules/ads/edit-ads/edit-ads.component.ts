08184fbad97a475a4f495553aeedb2776558fba3
ahmed.elhady@shgardi.app <FUTURE@DESKTOP-VDPH0E1>
2021-03-31 00:33:48 +0200

    all changes

--- src/app/modules/ads/edit-ads/edit-ads.component.ts
+++ src/app/modules/ads/edit-ads/edit-ads.component.ts
@@ -7,11 +7,11 @@ import { Observable } from 'rxjs';
 import { BaseComponent } from 'src/app/@core/Component/BaseComponent/BaseComponent';
 import { GeoLocationService } from 'src/app/shared/services/geo-location.service';
 import { AppConsts } from 'src/AppConsts';
-import { AdvertisementServiceProxy,ServiceProxy, CitiesServiceProxy, CountriesServiceProxy, CreateAdvertisementCommand, RegionManagementServiceProxy, GetServiceTypeListCommand, ApplyForAdvertisementCommand, AdsDto, SpaceInfoDto, ServiceTypeDto, EditAdvertisementCommand } from 'src/shared/service-proxies/service-proxies';
+import { AdvertisementServiceProxy, ServiceProxy, CitiesServiceProxy, CountriesServiceProxy, CreateAdvertisementCommand, RegionManagementServiceProxy, GetServiceTypeListCommand, ApplyForAdvertisementCommand, AdsDto, SpaceInfoDto, ServiceTypeDto, EditAdvertisementCommand } from 'src/shared/service-proxies/service-proxies';
 import { AddLocationComponent } from '../add-ads/add-location/add-location.component';
 
 export interface ImageInfo {
-  
+
   imageUrl: string;
   imageName: string;
   imageExtention: string;
@@ -24,24 +24,24 @@ export interface ImageInfo {
   styleUrls: ['./edit-ads.component.scss']
 })
 export class EditAdsComponent implements OnInit {
-  service:any[];
+  service: any[];
   @ViewChild("CommitModel", { static: false }) CommitModel;
   @Output() next = new EventEmitter<any>();
-  secondFormGroup: FormGroup; 
-  
+  secondFormGroup: FormGroup;
+
   file2: File[];
   imageInfo2: ImageInfo[] = [];
-  AdCategoryList : any[] = [
-    {id : 0, name :"Fixed"},
-    {id : 1, name : "movable"},
-    {id : 2, name :  "Digital"},
-    {id : 3, name :  "SocialMedia"},
-   
+  AdCategoryList: any[] = [
+    { id: 0, name: "Fixed" },
+    { id: 1, name: "movable" },
+    { id: 2, name: "Digital" },
+    { id: 3, name: "SocialMedia" },
+
   ];
-  AdCategory:any;
+  AdCategory: any;
   isAuctionable: boolean = false;
   latitude = 24.7444191;
-  longitude =46.8323063;
+  longitude = 46.8323063;
   durations = [
     {
       id: 1,
@@ -65,119 +65,114 @@ export class EditAdsComponent implements OnInit {
     },
   ];
   id: any;
-  AdDto:AdsDto[];
-images: string[];
-baseUrl = AppConsts.baseUrlImage;
-ApplyForAdvertisementCommand:ApplyForAdvertisementCommand=new ApplyForAdvertisementCommand; 
-EditAdvertisementCommand :EditAdvertisementCommand=new EditAdvertisementCommand;
+  AdDto: AdsDto[];
+  images: string[];
+  baseUrl = AppConsts.baseUrlImage;
+  ApplyForAdvertisementCommand: ApplyForAdvertisementCommand = new ApplyForAdvertisementCommand;
+  EditAdvertisementCommand: EditAdvertisementCommand = new EditAdvertisementCommand;
 
   countries = [];
   cities = [];
   selectedCountry = 1;
   selectedCity = 1;
   selectedDuration = 2;
-  GetServiceTypeListCommand=new GetServiceTypeListCommand();
-  services = [  
+  GetServiceTypeListCommand = new GetServiceTypeListCommand();
+  services = [
   ];
-  Images:any;
-Model=new CreateAdvertisementCommand;
+  Images: any;
+  Model = new CreateAdvertisementCommand;
   ctrls: FormControl[];
   AdvertisementDetailDto: SpaceInfoDto;
   constructor(
-    private _formBuilder: FormBuilder, 
+    private _formBuilder: FormBuilder,
     public dialog: MatDialog,
-    private router : Router,
-    private CitiesService :CitiesServiceProxy,
-    private Service : RegionManagementServiceProxy, private ServiceProxy:  ServiceProxy,private activatedRoute: ActivatedRoute,
-    private AdvertisementService:AdvertisementServiceProxy,private _snackBar: MatSnackBar,private Location:GeoLocationService
+    private router: Router,
+    private CitiesService: CitiesServiceProxy,
+    private Service: RegionManagementServiceProxy, private ServiceProxy: ServiceProxy, private activatedRoute: ActivatedRoute,
+    private AdvertisementService: AdvertisementServiceProxy, private _snackBar: MatSnackBar, private Location: GeoLocationService
 
-    ){
-      this.service=new Array;
-   // super();
+  ) {
+    this.service = new Array;
+    // super();
   }
   ngOnInit() {
     this.LoadServiceTypeList();
     this.LoadCountries();
-  this.secondFormGroup = this._formBuilder.group({
-    title: ['', Validators.required],
-    price: ['', Validators.required],
-    isAuction:[true],
-    auctionDays: [1],
-    description: ['', Validators.required],
-    countryId: ['', Validators.required],
-    cityId: ['', Validators.required],
-    lat: [null],
-    lng: [null],    
-    images:[[],[]],
-    address:[""],
-    fromDate:new FormControl(new Date()),
-    toDate:new FormControl(new Date()),
-    FreeServiceIds:this._formBuilder.array([]),
-    adCategory: ['', Validators.required],
-  });
+    this.secondFormGroup = this._formBuilder.group({
+      title: ['', Validators.required],
+      price: ['', Validators.required],
+      isAuction: [true],
+      auctionDays: [1],
+      description: ['', Validators.required],
+      countryId: ['', Validators.required],
+      cityId: ['', Validators.required],
+      lat: [null],
+      lng: [null],
+      images: [[], []],
+      address: [""],
+      fromDate: new FormControl(new Date()),
+      toDate: new FormControl(new Date()),
+      FreeServiceIds: this._formBuilder.array([]),
+      adCategory: ['', Validators.required],
+    });
 
     this.activatedRoute.queryParams.subscribe(parm => {
       let querySting = parm['id'];
       if (querySting) {
         this.id = querySting;
-        this.ApplyForAdvertisementCommand.adId=this.id;
-             this.AdvertisementService.getAdvertisementById(this.id).subscribe(
+        this.ApplyForAdvertisementCommand.adId = this.id;
+        this.AdvertisementService.getAdvertisementById(this.id).subscribe(
           (result) => {
-           
             console.log(result);
-            
-            ;
             this.secondFormGroup = this._formBuilder.group({
               title: [result.title, Validators.required],
               price: [result.price, Validators.required],
-              isAuction:[false],
+              isAuction: [false],
               auctionDays: [1],
               description: [result.description, Validators.required],
               countryId: [result.countryId, Validators.required],
               cityId: [result.cityId, Validators.required],
               lat: [result.lat],
-              lng: [result.lng],    
-              images:[result.images,[]],
-              address:[result.address],
-              fromDate:new FormControl(result.fromDate),
-              toDate:new FormControl(result.toDate),
-              FreeServiceIds:new FormArray([]),
+              lng: [result.lng],
+              images: [result.images, []],
+              address: [result.address],
+              fromDate: new FormControl(result.fromDate),
+              toDate: new FormControl(result.toDate),
+              FreeServiceIds: new FormArray([]),
               adCategory: [result.adCategory, Validators.required],
-             
+
             });
-            if(result.cityId!=null){
+            if (result.cityId != null) {
               this.Loadcities(result.countryId);
             }
-this.images=result.images;
-            if(result.images.length>0){
-              for(var i=0;i<result.images.length;i++){
+            this.images = result.images;
+            if (result.images.length > 0) {
+              for (var i = 0; i < result.images.length; i++) {
                 this.imageInfo2.push({
                   imageName: result.images[i],
                   imageSize: "",
-                  imageUrl: this.baseUrl+result.images[i]+'?w=100&h=100',
+                  imageUrl: this.baseUrl + result.images[i] + '?w=100&h=100',
                   imageExtention: ""
-                 
                 })
               }
-             
             }
             debugger
-             var list:any=result.freeServices.map(x=>x.serviceTypeId);
-           
+            var list: any = result.freeServices.map(x => x.serviceTypeId);
 
-            this.services.map((perm,i) => {
+
+            this.services.map((perm, i) => {
               debugger
               //  this.permissionsArr.at(i).patchValue(true)
-              if(list.find(x=>x == perm.id) != null){
-                let obj:any={name:perm.name.ar,value:true,serviceTypeId:perm.id};
+              if (list.find(x => x == perm.id) != null) {
+                let obj: any = { name: perm.name.ar, value: true, serviceTypeId: perm.id };
                 this.service.push(obj)
                 //this.permissionsArr.at(i).patchValue(true)
-              }else{
-                let obj:any={name:perm.name.ar,value:false,serviceTypeId:perm.id};
+              } else {
+                let obj: any = { name: perm.name.ar, value: false, serviceTypeId: perm.id };
                 this.service.push(obj)
               }
-              
-              
+
+
             })
           },
           (err) => {
@@ -187,21 +182,19 @@ this.images=result.images;
       }
     });
 
-  
-   
-   
+
+
+
   }
- 
   get permissionsArr() {
     return this.secondFormGroup.get('FreeServiceIds') as FormArray;
   }
- 
   errorOccured(err: any) {
     throw new Error('Method not implemented.');
   }
   onCheckboxChange(e) {
     const checkArray: FormArray = this.secondFormGroup.get('FreeServiceIds') as FormArray;
-  debugger
+    debugger
     if (e.target.checked) {
       checkArray.push(new FormControl(e.target.value));
     } else {
@@ -216,35 +209,35 @@ this.images=result.images;
     }
     console.log(this.secondFormGroup.get('FreeServiceIds').value)
   }
-  LoadCountries(){
+  LoadCountries() {
 
-    return this.Service.countries(1,150,"","","").subscribe(res=>{
-      this.countries=res.items;
+    return this.Service.countries(1, 150, "", "", "").subscribe(res => {
+      this.countries = res.items;
       console.log(this.countries)
     })
-   
-    
+
+
   }
   Change(countryid) {
-   
-   this.Loadcities(countryid);
-}
-  Loadcities(countryId){
 
-    return this.CitiesService.getCitiesByCountryId(countryId).subscribe(res=>{
-      this.cities=res;
+    this.Loadcities(countryid);
+  }
+  Loadcities(countryId) {
+
+    return this.CitiesService.getCitiesByCountryId(countryId).subscribe(res => {
+      this.cities = res;
       console.log(this.cities)
     })
 
-    
-  }
-  LoadServiceTypeList(){
 
-    return this.ServiceProxy.getServiceTypesList(this.GetServiceTypeListCommand).subscribe(res=>{
-      this.services=res;
-    
-     console.log(this.services)
-    }) 
+  }
+  LoadServiceTypeList() {
+
+    return this.ServiceProxy.getServiceTypesList(this.GetServiceTypeListCommand).subscribe(res => {
+      this.services = res;
+
+      console.log(this.services)
+    })
   }
   processDataFile2(fileInput: any) {
     ;
@@ -258,23 +251,23 @@ this.images=result.images;
         let url
         reader.readAsDataURL(element);
         reader.onload = () => {
-         
+
           url = reader.result.toString();
-     
+
         };
 
 
 
         if (this.imageInfo2.length < 4) {
           setTimeout(() => {
-           ;
+            ;
             this.imageInfo2.push({
               imageName: element.name,
               imageSize: size.toString(),
               imageUrl: url,
               imageExtention: ""
             })
-          //  this.secondFormGroup.get('images').setValue(url);
+            //  this.secondFormGroup.get('images').setValue(url);
             console.log()
             console.log(this.imageInfo2)
           }, 200);
@@ -283,107 +276,89 @@ this.images=result.images;
       }
     }
   }
-
-  
   removeAttachments2(e) {
     ;
     this.imageInfo2.splice(e, 1)
   }
-  
-  onChange($event){
+  onChange($event) {
     this.isAuctionable = !this.isAuctionable;
     console.log(this.secondFormGroup.value.isAuction);
   }
-
   openAddLocationDialog() {
     const dialogRef = this.dialog.open(AddLocationComponent, {
       width: '70%'
     });
     dialogRef.afterClosed().subscribe(data => {
-     
+
       console.log('Child component\'s event was triggered', data);
       this.secondFormGroup.get('lat').setValue(data.latitude);
       this.secondFormGroup.get('lng').setValue(data.longitude);
       this.secondFormGroup.get('address').setValue(data.address);
-   });
+    });
   }
- 
-
-
-  
-  // submitForm(){
-  //   if(this.secondFormGroup.valid){
-  //     console.log('try to submit'); 
-  //     this.showAdsStepper = false;
-  //     this.showAddingAdsSuccessMsg = true;
-  //     this.showUserServices = true;
-  //   }   
-  // }
   get fc() {
     return this.secondFormGroup.controls;
   }
   nextstep() {
     ;
     if (this.secondFormGroup.valid) {
-     ;
-    debugger
+      ;
+      debugger
       this.secondFormGroup.removeControl('countryId');
       let formArray = this.secondFormGroup.controls['images'] as FormArray;
-      var res=this.imageInfo2.map(x=>x.imageUrl);
-      this.images.map((perm,i) => {
+      var res = this.imageInfo2.map(x => x.imageUrl);
+      this.images.map((perm, i) => {
         debugger
-     
-        if(res.find(x=>x ==this.baseUrl+ perm+'?w=100&h=100')){
-        
-      res.splice(i,1);
-        
+
+        if (res.find(x => x == this.baseUrl + perm + '?w=100&h=100')) {
+
+          res.splice(i, 1);
+
         }
       })
       formArray.patchValue(res);
       const checkArray: FormArray = this.secondFormGroup.get('FreeServiceIds') as FormArray;
-      var cc=this.service.filter(x=>x.value==true);
+      var cc = this.service.filter(x => x.value == true);
       cc.forEach(element => {
         checkArray.push(new FormControl(element.serviceTypeId));
       });
-    
-     
-     console.log("بطيخ",this.service)
-     console.log("سسس",this.secondFormGroup.value)
-         this.EditAdvertisementCommand=this.secondFormGroup.value;
-         this.EditAdvertisementCommand.id=this.id;
-         if(this.secondFormGroup.value.lng===null||this.secondFormGroup.value.lng==="0")
-         this.EditAdvertisementCommand.lng=this.longitude;
-         if(this.secondFormGroup.value.lat===null||this.secondFormGroup.value.lat==="0")
-         this.EditAdvertisementCommand.lat=this.latitude;
-      this.AdvertisementService.editAdvertisement(this.EditAdvertisementCommand).subscribe( 
-        res=>{
-        if(res!==null)
-        {
-          this._snackBar.open("تم الاضافة بنجاح","اضافة" ,{
-            duration: 2220,
-            
-          });
-         // this.router.navigateByUrl('/');
-         this.router.navigateByUrl(
-          '/ads/UserServices'
-        );
-          let adsData = {
-            secondFormGroup: this.secondFormGroup.value
+
+
+      console.log("بطيخ", this.service)
+      console.log("سسس", this.secondFormGroup.value)
+      this.EditAdvertisementCommand = this.secondFormGroup.value;
+      this.EditAdvertisementCommand.id = this.id;
+      if (this.secondFormGroup.value.lng === null || this.secondFormGroup.value.lng === "0")
+        this.EditAdvertisementCommand.lng = this.longitude;
+      if (this.secondFormGroup.value.lat === null || this.secondFormGroup.value.lat === "0")
+        this.EditAdvertisementCommand.lat = this.latitude;
+      this.AdvertisementService.editAdvertisement(this.EditAdvertisementCommand).subscribe(
+        res => {
+          if (res !== null) {
+            this._snackBar.open("تم الاضافة بنجاح", "اضافة", {
+              duration: 2220,
+
+            });
+            // this.router.navigateByUrl('/');
+            this.router.navigateByUrl(
+              '/ads/UserServices'
+            );
+            let adsData = {
+              secondFormGroup: this.secondFormGroup.value
+            }
+            this.next.emit(adsData);
+
           }
-          this.next.emit(adsData);
-        
-        }
-        else
-        {
-          this._snackBar.open("حدث خطأ عند الاضافة","الاضافة" ,{
-            duration: 2220,
-            
-          });
-        }
-     
-      })
-    
-    
+          else {
+            this._snackBar.open("حدث خطأ عند الاضافة", "الاضافة", {
+              duration: 2220,
+
+            });
+          }
+
+        })
+
+
     }
   }
 }
