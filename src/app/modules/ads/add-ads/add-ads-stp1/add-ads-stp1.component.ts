b6b71ac91e4166f0bd8bef7a092cdfe0f3cfdb54
Mahmoud Abd-Elradi <mahmoudabd23451@gmail.com>
2021-06-02 00:06:32 +0200

    لم يحدد خيارات صيغة  الصورة

--- src/app/modules/ads/add-ads/add-ads-stp1/add-ads-stp1.component.ts
+++ src/app/modules/ads/add-ads/add-ads-stp1/add-ads-stp1.component.ts
@@ -1,13 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Injector, OnInit, Output, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { MatDialog } from '@angular/material/dialog';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthenticationService } from 'src/app/@core/auth/authentication.service';
-import { BaseComponent } from 'src/app/@core/Component/BaseComponent/BaseComponent';
 import { LoginComponent } from 'src/app/modules/auth/login/login.component';
 import { GeoLocationService } from 'src/app/shared/services/geo-location.service';
+import { AppConsts } from 'src/AppConsts';
 import { AdvertisementServiceProxy,ServiceProxy, CitiesServiceProxy, CountriesServiceProxy, CreateAdvertisementCommand, RegionManagementServiceProxy, GetServiceTypeListCommand, AddCommercialRecordCommand, AuthServiceProxy, IsUserHasCommercialRecordCommand } from 'src/shared/service-proxies/service-proxies';
 import { AddLocationComponent } from '../add-location/add-location.component';
 
@@ -28,11 +27,11 @@ export class AddAdsStp1Component  implements OnInit {
   firstFormGroup: FormGroup; 
   file: File[];
   imageInfo: ImageInfo[] = [];
-
+  BaseFile: any;
   @Output() next = new EventEmitter<any>();
   AddCommercialRecordCommand:AddCommercialRecordCommand={} as AddCommercialRecordCommand;
   IsUserHasCommercialRecordCommand:IsUserHasCommercialRecordCommand=new IsUserHasCommercialRecordCommand()
-  constructor(  private _formBuilder: FormBuilder, 
+  constructor(  private _formBuilder: FormBuilder, private http: HttpClient,
     public dialog: MatDialog,
     private router : Router,
     private CitiesService :CitiesServiceProxy,
@@ -43,8 +42,9 @@ export class AddAdsStp1Component  implements OnInit {
   }
 
   ngOnInit() {
+    this.BaseFile=AppConsts.baseUrlImage;
     this.firstFormGroup = this._formBuilder.group({
-      commercialRegisterAttachments: [''],
+      images: [''],
       File: [null],
     });
     var login=localStorage.getItem('isAuthenticated');
@@ -125,17 +125,61 @@ Check(){
 
   
   removeAttachments(e) {
-    this.imageInfo.splice(e, 1);
+    this.ImageUrl.splice(e, 1);
   }
+  UploadImage2(formData){
+ 
+    return  this.http.post(AppConsts.baseUrl + '/api/UploadFile/FileUpload', formData);
+     
+  }
+  getFileExtension3(filename) {
+debugger
+    return filename.split('.').pop();
+  }
+  ImageUrl: any;
+  fileToUpload = null;
+  file2: File[];
+  uploadImage(event) 
+{
+debugger
+ this.file2 = event.target.files;
+ 
+  const formData = new FormData();
+ for (let index = 0; index < this.file2.length; index++) {
+  if(this.getFileExtension3(this.file2[index].name.toUpperCase())=="PNG"||this.getFileExtension3(this.file2[index].name.toUpperCase())=="JPG"||this.getFileExtension3(this.file2[index].name.toUpperCase())=="JPEG"||this.getFileExtension3(this.file2[index].name.toUpperCase())=="BMP"||this.getFileExtension3(this.file2[index].name.toUpperCase())=="GIF")
 
+   formData.append('files', this.file2[index]);
+   else{
+     this._snackBar.open(" هناك خط في امتداد الصورة او الملف","الاضافة",{
+        duration: 2220,
+        
+      });
+   }
+}
+  this.UploadImage2(formData).subscribe(event => {
+debugger
+ const result= event as any;
+console.log(result)
+if(this.ImageUrl==undefined||this.ImageUrl==null){
+  this.ImageUrl=result.filePaths;
+}
+else
+{
+  this.ImageUrl.push.apply(this.ImageUrl,result.filePaths)
+
+}
+
+  }
+  );
+} 
   saveData(){
-    ;
-    let formArray = this.firstFormGroup.controls['commercialRegisterAttachments'] as FormArray;
-    formArray.patchValue(this.imageInfo.map(x=>x.imageUrl));
-
-    this.AddCommercialRecordCommand.images=this.imageInfo.map(x=>x.imageUrl);
-    if(this.AddCommercialRecordCommand.images.length>0){
-      this.AdvertisementService.addCommercialRecord(this.AddCommercialRecordCommand).subscribe( 
+   debugger
+    let formArray = this.firstFormGroup.controls['images'] as FormArray;
+    //formArray.patchValue(this.imageInfo.map(x=>x.imageUrl));
+    formArray.patchValue(this.ImageUrl);
+  //  this.AddCommercialRecordCommand.images=formArray.value;
+    if(this.firstFormGroup.valid){
+      this.AdvertisementService.addCommercialRecord(this.firstFormGroup.value).subscribe( 
         res=>{
         if(res!==null)
         {
